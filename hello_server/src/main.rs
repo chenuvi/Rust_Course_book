8337f88018d9d632914646202067d699edb4dd3b
chenuvi <chenuvi@outlook.com>
2024-05-24 23:03:30 +0800

    Feat: 返回 HTML 页面

--- hello_server/src/main.rs
+++ hello_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -20,6 +21,14 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // stream.write_all(response.as_bytes()).unwrap();
+
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    // TODO 不渲染 html
+    let content_type = "Content-Type:text/html; charset=utf-8";
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n{content_type}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
